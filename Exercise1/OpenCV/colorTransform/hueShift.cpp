78c3d0a81615f88f141f60b492f9ac513c69b106
Emmanuel Bustos <ema2159@gmail.com>
2021-04-20 13:51:45 +0200

    Add: Hue shift transformation implementation

---
+++ Exercise1/OpenCV/colorTransform/hueShift.cpp
@@ -0,0 +1,55 @@
+#include <chrono> // for high_resolution_clock
+#include <iostream>
+#include <math.h>
+#include <omp.h>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+cv::Vec3f hueShift(cv::Vec3f pix, float angle) {
+  float C_ab = sqrt(pow(pix[1], 2) + pow(pix[2], 2)); // sqrt((a*)^2+(b*)^2)
+  float h = atan2(pix[2], pix[1]);
+
+  // Shift CIELAB color's a* and b* components
+  h += (angle * M_PI) / 180.0;
+
+  return cv::Vec3f(pix[0], cos(h) * C_ab, sin(h) * C_ab);
+}
+
+int main(int argc, char **argv) {
+
+  // Get images from command line
+  cv::Mat source = cv::imread(argv[1], cv::IMREAD_COLOR);
+  float hue_shift_angle = atof(argv[2]);
+
+  cv::imshow("Source Image", source);
+
+  // Placeholder image for the result
+  cv::Mat destination(source.rows, source.cols, CV_8UC3, cv::Scalar(0, 255, 0));
+
+  cv::cvtColor(source, source, cv::COLOR_BGR2Lab);
+
+  auto begin = chrono::high_resolution_clock::now();
+
+#pragma omp parallel for
+  for (int i = 0; i < source.rows; i++) {
+    // #pragma omp parallel for
+    for (int j = 0; j < source.cols; j++) {
+
+      destination.at<cv::Vec3b>(i, j) =
+          hueShift((cv::Vec3f)source.at<cv::Vec3b>(i, j), hue_shift_angle);
+    }
+  }
+
+  cv::cvtColor(destination, destination, cv::COLOR_Lab2BGR);
+
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> diff = end - begin;
+
+  cv::imshow("Processed Image", destination);
+
+  cout << "Processing time: " << diff.count() << " s" << endl;
+
+  cv::waitKey();
+  return 0;
+}
