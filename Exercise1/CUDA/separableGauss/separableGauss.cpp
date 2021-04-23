e30d8c806e86fbcefb52f12ec5fc161ad8be266d
Emmanuel Bustos <ema2159@gmail.com>
2021-04-24 00:32:37 +0200

    Add: Separable Gauss filter implementation

--- Exercise1/CUDA/separableGauss/separableGauss.cpp
+++ Exercise1/CUDA/separableGauss/separableGauss.cpp
@@ -5,35 +5,43 @@
 
 using namespace std;
 
-void startCUDA(cv::cuda::GpuMat &src, cv::cuda::GpuMat &dst, int kernel_size, float sigma);
+void startCUDA(cv::cuda::GpuMat &src, cv::cuda::GpuMat &dst, int kernel_size,
+               float sigma, bool first_pass);
 
 int main(int argc, char **argv) {
   cv::namedWindow("Original Image", cv::WINDOW_OPENGL | cv::WINDOW_AUTOSIZE);
   cv::namedWindow("Processed Image", cv::WINDOW_OPENGL | cv::WINDOW_AUTOSIZE);
 
   cv::Mat h_img = cv::imread(argv[1]);
+  cv::Mat h_interm(h_img.rows, h_img.cols, CV_8UC3, cv::Scalar(0, 255, 0));
   cv::Mat h_result(h_img.rows, h_img.cols, CV_8UC3, cv::Scalar(0, 255, 0));
-  cv::cuda::GpuMat d_img, d_result;
+  cv::cuda::GpuMat d_img, d_interm, d_result;
 
   cv::imshow("Original Image", h_img);
 
   auto begin = chrono::high_resolution_clock::now();
   const int iter = 1;
 
-  const int KERNEL_SIZE = 31;
+  const int KERNEL_SIZE = atoi(argv[2]);
   const int KERNEL_DIV_2 = KERNEL_SIZE / 2;
-  const float SIGMA = 5;
+  const float SIGMA = atoi(argv[3]);
 
   // Create input image which corresponds to the source image with an added
   // replication padding.
-  cv::cuda::copyMakeBorder(h_img, h_img, KERNEL_DIV_2, KERNEL_DIV_2, KERNEL_DIV_2,
-		     KERNEL_DIV_2, cv::BORDER_REPLICATE);
+  cv::cuda::copyMakeBorder(h_img, h_img, KERNEL_DIV_2, KERNEL_DIV_2,
+                           KERNEL_DIV_2, KERNEL_DIV_2, cv::BORDER_REPLICATE);
+  cv::cuda::copyMakeBorder(h_interm, h_interm, KERNEL_DIV_2, KERNEL_DIV_2,
+                           KERNEL_DIV_2, KERNEL_DIV_2, cv::BORDER_REPLICATE);
 
   d_img.upload(h_img);
+  d_interm.upload(h_interm);
   d_result.upload(h_result);
 
   for (int i = 0; i < iter; i++) {
-    startCUDA(d_img, d_result, KERNEL_SIZE, SIGMA);
+    // First pass
+    startCUDA(d_img, d_interm, KERNEL_SIZE, SIGMA, true);
+    // Second pass
+    startCUDA(d_interm, d_result, KERNEL_SIZE, SIGMA, false);
   }
   auto end = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> diff = end - begin;
--- Exercise1/CUDA/separableGauss/separableGauss.cu
+++ Exercise1/CUDA/separableGauss/separableGauss.cu
@@ -8,21 +8,20 @@
 #include <opencv2/core/cuda/vec_math.hpp>
 
 /**
- * Returns the value of a 2-D Gaussian function with an standard
- * deviation of sigma at position (x, y)
+ * Returns the value of a Gaussian function with an standard
+ * deviation of sigma and an input value of x.
  *
- * @param x: x value of the Gaussian function.
- * @param y: y value of the Gaussian function.
+ * @param x: input value of the Gaussian function.
  * @param sigma: standard deviation of Gaussian function.
- * @return result of the 2-D Gaussian function.
+ * @return result of the Gaussian function.
  */
-__device__ float gauss_2D(int x, int y, float sigma) {
-  return exp(-((pow(x, 2.0) + pow(y, 2.0)) / (2.0 * pow(sigma, 2.0))));
+__device__ float gauss_func(int x, float sigma) {
+  return exp(-(pow(x, 2.0) / (2.0 * pow(sigma, 2.0))));
 }
 
 __global__ void process(const cv::cuda::PtrStep<uchar3> src,
 			cv::cuda::PtrStep<uchar3> dst, int rows, int cols,
-			int kernel_size, int sigma) {
+			int kernel_size, int sigma, bool first_pass) {
 
     const int dst_x = blockDim.x * blockIdx.x + threadIdx.x;
     const int dst_y = blockDim.y * blockIdx.y + threadIdx.y;
@@ -32,13 +31,22 @@ __global__ void process(const cv::cuda::PtrStep<uchar3> src,
 	// Generate gaussian kernel of size kernel_size
 	float3 val = make_float3(0, 0, 0);
 	float gauss_sum = 0;
-	for (int m = -kernel_div2; m <= kernel_div2; m++) {
-	    for (int n = -kernel_div2; n <= kernel_div2; n++) {
-		float gauss_val = gauss_2D(m, n, sigma);
+	if (first_pass) {
+	    for (int m = -kernel_div2; m <= kernel_div2; m++) {
+		float gauss_val = gauss_func(m, sigma);
 		gauss_sum += gauss_val;
-		val.x += (float)src(dst_y+kernel_div2+n, dst_x+kernel_div2+m).x*gauss_val;
-		val.y += (float)src(dst_y+kernel_div2+n, dst_x+kernel_div2+m).y*gauss_val;
-		val.z += (float)src(dst_y+kernel_div2+n, dst_x+kernel_div2+m).z*gauss_val;
+		val.x += (float)src(dst_y+kernel_div2, dst_x+kernel_div2+m).x*gauss_val;
+		val.y += (float)src(dst_y+kernel_div2, dst_x+kernel_div2+m).y*gauss_val;
+		val.z += (float)src(dst_y+kernel_div2, dst_x+kernel_div2+m).z*gauss_val;
+	    }
+	}
+	else {
+	    for (int n = -kernel_div2; n <= kernel_div2; n++) {
+		float gauss_val = gauss_func(n, sigma);
+		gauss_sum += gauss_val;
+		val.x += (float)src(dst_y+kernel_div2+n, dst_x+kernel_div2).x*gauss_val;
+		val.y += (float)src(dst_y+kernel_div2+n, dst_x+kernel_div2).y*gauss_val;
+		val.z += (float)src(dst_y+kernel_div2+n, dst_x+kernel_div2).z*gauss_val;
 	    }
 	}
 	val.x = val.x/gauss_sum;
@@ -55,11 +63,12 @@ int divUp(int a, int b) {
   return ((a % b) != 0) ? (a / b + 1) : (a / b);
 }
 
-void startCUDA ( cv::cuda::GpuMat& src, cv::cuda::GpuMat& dst, int KERNEL_SIZE, float SIGMA ) {
+void startCUDA (cv::cuda::GpuMat& src, cv::cuda::GpuMat& dst, int KERNEL_SIZE,
+		 float SIGMA, bool first_pass) {
   const dim3 block(32, 8);
   const dim3 grid(divUp(dst.cols, block.x), divUp(dst.rows, block.y));
 
-  process<<<grid, block>>>(src, dst, dst.rows, dst.cols, KERNEL_SIZE, SIGMA);
-
+  process<<<grid, block>>>(src, dst, dst.rows, dst.cols, KERNEL_SIZE, SIGMA,
+			   first_pass);
 }
 
