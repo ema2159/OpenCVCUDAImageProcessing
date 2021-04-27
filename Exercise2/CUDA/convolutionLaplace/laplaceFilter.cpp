fa1c7e87e16f7cb1b03f412ee524f9f77fbf63a1
Emmanuel Bustos <ema2159@gmail.com>
2021-04-27 12:55:32 +0200

    Add: Shared memory accelerated laplacian filter CUDA implementation

---
+++ Exercise2/CUDA/convolutionLaplace/laplaceFilter.cpp
@@ -0,0 +1,42 @@
+#include <chrono> // for high_resolution_clock
+#include <iostream>
+#include <opencv2/cudawarping.hpp>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+void startCUDA(cv::cuda::GpuMat &src, cv::cuda::GpuMat &dst);
+
+int main(int argc, char **argv) {
+  cv::namedWindow("Original Image", cv::WINDOW_OPENGL | cv::WINDOW_AUTOSIZE);
+  cv::namedWindow("Processed Image", cv::WINDOW_OPENGL | cv::WINDOW_AUTOSIZE);
+
+  cv::Mat h_img = cv::imread(argv[1]);
+  cv::Mat h_result(h_img.rows, h_img.cols, CV_8UC3, cv::Scalar(0, 255, 0));
+  cv::cuda::GpuMat d_img, d_result;
+
+  cv::imshow("Original Image", h_img);
+
+  auto begin = chrono::high_resolution_clock::now();
+  const int iter = 1;
+
+  d_img.upload(h_img);
+  d_result.upload(h_result);
+
+  for (int i = 0; i < iter; i++) {
+    startCUDA(d_img, d_result);
+  }
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> diff = end - begin;
+
+  cv::imshow("Processed Image", d_result);
+
+  cout << diff.count() << endl;
+  cout << diff.count() / iter << endl;
+  cout << iter / diff.count() << endl;
+
+  cv::waitKey();
+  return 0;
+
+  return 0;
+}
---
+++ Exercise2/CUDA/convolutionLaplace/laplaceFilter.cu
@@ -0,0 +1,88 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <opencv2/opencv.hpp>
+#include <cfloat>
+#include <opencv2/core/cuda/common.hpp>
+#include <opencv2/core/cuda/border_interpolate.hpp>
+#include <opencv2/core/cuda/vec_traits.hpp>
+#include <opencv2/core/cuda/vec_math.hpp>
+
+#define TILE_SIZE 15
+#define KERNEL_SIZE 3
+#define KERNEL_DIV2 1
+
+__device__ const float LAPLACE_KERNEL[3][3] = {{-1,-1,-1},
+					       {-1, 8,-1},
+					       {-1,-1,-1}};
+
+/**
+ * Clamps the value val in the interval [lo, high].
+ * Equivalent to max(lo, min(val, high)).
+ *
+ * @param val: value to clamp.
+ * @param lo: lower bound for the clamping.
+ * @param high: higher bound for the clamping.
+ * @return val clamped between lo and high.
+ */
+__device__ int clamp(int val, int lo, int high) {
+  return max(lo, min(val, high));
+}
+
+__global__ void process(const cv::cuda::PtrStep<uchar3> src,
+			cv::cuda::PtrStep<uchar3> dst, int rows, int cols) {
+
+    const int dst_x = TILE_SIZE * blockIdx.x + threadIdx.x-KERNEL_SIZE;
+    const int dst_y = TILE_SIZE * blockIdx.y + threadIdx.y-KERNEL_SIZE;
+
+    // Create shared memory using externally passed size
+    extern __shared__ uchar3 tile[];
+
+    int px = clamp(dst_x, 0, cols-1);
+    int py = clamp(dst_y, 0, rows-1);
+
+    // Cache pixels in shared memory
+    tile[threadIdx.y*(TILE_SIZE+KERNEL_SIZE)+threadIdx.x] = src(py, px);
+
+    // Wait until all thread cache their pixes values
+    __syncthreads();  
+
+    bool is_inside_tile =
+        KERNEL_DIV2 <= threadIdx.x && threadIdx.x < TILE_SIZE + KERNEL_DIV2 &&
+        KERNEL_DIV2 <= threadIdx.y && threadIdx.y < TILE_SIZE + KERNEL_DIV2;
+    if (dst_x < cols && dst_y < rows && is_inside_tile) {
+	float3 val = make_float3(0, 0, 0);
+	float gauss_sum = 0;
+        for (int m = -KERNEL_DIV2; m <= KERNEL_DIV2; m++) {
+            for (int n = -KERNEL_DIV2; n <= KERNEL_DIV2; n++) {
+                int ty = threadIdx.y+n;
+                int tx = threadIdx.x+m;
+                uchar3 pix = tile[ty*(TILE_SIZE+KERNEL_SIZE)+tx];
+		val.x += (float)pix.x
+		    *LAPLACE_KERNEL[m+KERNEL_DIV2][n+KERNEL_DIV2];
+		val.y += (float)pix.y
+		    *LAPLACE_KERNEL[m+KERNEL_DIV2][n+KERNEL_DIV2];
+		val.z += (float)pix.z
+		    *LAPLACE_KERNEL[m+KERNEL_DIV2][n+KERNEL_DIV2];
+	    }
+        }
+
+        dst(dst_y, dst_x).x = val.x;
+        dst(dst_y, dst_x).y = val.y;
+        dst(dst_y, dst_x).z = val.z;
+    }
+}
+
+int divUp(int a, int b) {
+    return ((a % b) != 0) ? (a / b + 1) : (a / b);
+}
+
+void startCUDA ( cv::cuda::GpuMat& src, cv::cuda::GpuMat& dst) {
+    const dim3 block(TILE_SIZE+KERNEL_SIZE, TILE_SIZE+KERNEL_SIZE);
+    const dim3 grid(divUp(dst.cols, TILE_SIZE)+1, divUp(dst.rows, TILE_SIZE)+1);
+
+  
+    // Create a tile to process pixels within a block's shared memory
+    int shmem_size = sizeof(uchar3)*(TILE_SIZE+KERNEL_SIZE)*(TILE_SIZE+KERNEL_SIZE);
+  
+    process<<<grid, block, shmem_size>>>(src, dst, dst.rows, dst.cols);
+}
