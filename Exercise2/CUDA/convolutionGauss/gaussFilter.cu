d1c9ee47b7aac41a9f0296f8c17e9e24dd62c257
Emmanuel Bustos <ema2159@gmail.com>
2021-04-27 10:20:34 +0200

    Mod: Made shared memory gaussian partially work

--- Exercise2/CUDA/convolutionGauss/gaussFilter.cu
+++ Exercise2/CUDA/convolutionGauss/gaussFilter.cu
@@ -7,7 +7,7 @@
 #include <opencv2/core/cuda/vec_traits.hpp>
 #include <opencv2/core/cuda/vec_math.hpp>
 
-#define TILE_SIZE 20
+#define TILE_SIZE 15
 
 /**
  * Returns the value of a 2-D Gaussian function with an standard
@@ -26,8 +26,8 @@ __global__ void process(const cv::cuda::PtrStep<uchar3> src,
 			cv::cuda::PtrStep<uchar3> dst, int rows, int cols,
 			int kernel_size, int sigma) {
 
-    const int dst_x = blockDim.x * blockIdx.x + threadIdx.x;
-    const int dst_y = blockDim.y * blockIdx.y + threadIdx.y;
+    const int dst_x = (blockDim.x-kernel_size) * blockIdx.x + threadIdx.x;
+    const int dst_y = (blockDim.y-kernel_size) * blockIdx.y + threadIdx.y;
 
     // Filter radius
     const int kernel_div2 = kernel_size / 2;
@@ -39,12 +39,15 @@ __global__ void process(const cv::cuda::PtrStep<uchar3> src,
     int py = dst_y;
 
     // Cache pixels in shared memory
-    tile[threadIdx.y*(TILE_SIZE+kernel_div2)+threadIdx.x] = src(py, cols+px);
+    tile[threadIdx.y*(TILE_SIZE+kernel_size)+threadIdx.x] = src(py, cols+px);
 
     // Wait until all thread cache their pixes values
     __syncthreads();  
 
-    if (dst_x < cols-kernel_div2 && dst_y < rows-kernel_div2) {
+    bool is_inside_tile =
+	kernel_div2 <= threadIdx.x && threadIdx.x < TILE_SIZE + kernel_div2 &&
+	kernel_div2 <= threadIdx.y && threadIdx.y < TILE_SIZE + kernel_div2;
+    if (dst_x < cols-kernel_div2 && dst_y < rows-kernel_div2 && is_inside_tile) {
 	float3 val = make_float3(0, 0, 0);
 	float gauss_sum = 0;
 	for (int m = -kernel_div2; m <= kernel_div2; m++) {
@@ -54,10 +57,10 @@ __global__ void process(const cv::cuda::PtrStep<uchar3> src,
 
 		int ty = threadIdx.y+n;
 		int tx = threadIdx.x+m;
-		uchar3 pix = tile[0];
-		val.x += (float)pix.x*gauss_val;
-		val.y += (float)pix.y*gauss_val;
-		val.z += (float)pix.z*gauss_val;
+		uchar3 pix = tile[ty*(TILE_SIZE+kernel_size)+tx];
+		val.x += (float)pix.z*gauss_val;
+		val.y += (float)pix.x*gauss_val;
+		val.z += (float)pix.y*gauss_val;
 	    }
 	}
 	val.x = val.x/gauss_sum;
@@ -76,12 +79,15 @@ int divUp(int a, int b) {
 
 void startCUDA (cv::cuda::GpuMat& src, cv::cuda::GpuMat& dst, int KERNEL_SIZE,
 		float SIGMA ) {
-  const dim3 block(TILE_SIZE+KERNEL_SIZE/2, TILE_SIZE+KERNEL_SIZE/2);
-  const dim3 grid(divUp(dst.cols, block.x), divUp(dst.rows, block.y));
+  const dim3 block(TILE_SIZE+KERNEL_SIZE, TILE_SIZE+KERNEL_SIZE);
+  const dim3 grid(divUp(src.cols, block.x), divUp(src.rows, block.y));
 
   
   // Create a tile to process pixels within a block's shared memory
-  int shmem_size = pow((TILE_SIZE+KERNEL_SIZE/2), 2);
+  int shmem_size = sizeof(uchar3)*(TILE_SIZE+KERNEL_SIZE)*(TILE_SIZE+KERNEL_SIZE);
+  // printf("AAAAAAAAAAAAAAA %i\n", TILE_SIZE);
+  // printf("AAAAAAAAAAAAAAA %i\n", KERNEL_SIZE);
+  // printf("AAAAAAAAAAAAAAA %i\n", shmem_size);
   
   process<<<grid, block, shmem_size>>>(src, dst, src.rows, src.cols, KERNEL_SIZE, SIGMA);
 
